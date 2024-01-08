ab74fc71dca859b8b60faffcc2e5bfe420b5fae2
pratham182 <arorapratham900@gmail.com>
2024-01-08 23:27:19 +0530

    new changes

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const app=express();
 
 const port=process.env.PORT||1000;
 
+
 app.listen(port,()=>{
     console.log(`App is listening on port no ${port}`);
 })
--- frontend/app.js
+++
@@ -1,7 +0,0 @@
-import react from "react";
-import reactdom from "react-dom/client";
-
-
-const root=reactdom.createRoot(document.getElementById("root"));
-
-root.render(<h1>Hellos</h1>)
